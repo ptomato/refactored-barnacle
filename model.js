733f05ea9c31486c8880cd76ab3b0be869280f18
Philip Chimento <philip@endlessm.com>
2018-08-13 11:36:09 -0400

    model: Override all font sizes
    Copies a bunch of rules from the default theme and the Thematic theme,
    adjusting their font sizes.
    
    Also adds a Web CSS override, to control the font sizes inside the
    article. (This doesn't work yet for the Dinosaurs app because of an
    omission in the API: https://phabricator.endlessm.com/T23560)

--- model.js
+++ model.js
@@ -138,8 +138,197 @@ $logo-font: 'Patrick Hand SC';
 .set-page .Card__title {
     font-weight: bold;
     font-size: ${this._fontSize * 0.338}em;
-}`;
+}
+
+.LayoutSidebar .sidebar .ContentGroupNoResultsMessage {
+    &__title {
+        font-size: ${this._fontSize * 3}px;
+    }
+
+    &__subtitle {
+        font-size: ${this._fontSize * 2}px;
+    }
+}
+
+.CardDefault {
+    &__title {
+        font-size: ${this._fontSize * 1.8}px;
+    }
+
+    &__synopsis {
+        font-size: ${this._fontSize * 1.6}px;
+    }
+
+    &__context {
+        font-size: ${this._fontSize * 1.4}px;
+    }
+
+    &.CardText {
+        &.width-h,
+        &.width-g,
+        &.width-f,
+        &.width-e.height-e {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 4.8}px;
+            }
+            .CardDefault__synopsis {
+                font-size: ${this._fontSize * 2}px;
+            }
+        }
+
+        &.width-e.height-d,
+        &.width-e.height-c {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 3.68}px;
+            }
+        }
+
+        &.width-e,
+        &.width-d {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2.72}px;
+            }
+        }
+
+        &.width-c {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2.304}px;
+            }
+        }
+
+        &.width-b {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 1.8}px;
+            }
+        }
+
+        &.height-a {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 1.6}px;
+            }
+        }
+    }
+
+    &.CardPolaroid {
+        &.width-h,
+        &.width-g,
+        &.width-f,
+        &.width-e.height-e {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 3.68}px;
+            }
+        }
+
+        &.width-h.height-b,
+        &.width-g.height-b,
+        &.width-f.height-b,
+        &.width-e,
+        &.width-d.height-e,
+        &.width-d.height-d {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2.4}px;
+            }
+        }
+
+        &.width-d,
+        &.width-c,
+        &.width-b.height-e,
+        &.width-b.height-d {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2}px;
+            }
+        }
+
+        &.width-b,
+        &.height-a {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 1.6}px;
+            }
+        }
+    }
+
+    &.CardPost {
+        .CardDefault__context {
+            font-size: ${this._fontSize * 1.4}px;
+        }
+
+        &.width-h,
+        &.width-g {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 4.8}px;
+            }
+        }
+
+        &.width-h.height-c,
+        &.width-g.height-c,
+        &.width-f,
+        &.width-e.height-e {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 3.68}px;
+            }
+        }
+
+        &.width-g.height-b,
+        &.width-h.height-b,
+        &.width-f.height-b,
+        &.width-e,
+        &.width-d.height-e,
+        &.width-d.height-d {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2.4}px;
+            }
+        }
+
+        &.width-d,
+        &.width-c,
+        &.width-b.height-e,
+        &.width-b.height-d {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 2}px;
+            }
+        }
+
+        &.width-b,
+        &.height-a {
+            .CardDefault__title {
+                font-size: ${this._fontSize * 1.6}px;
+            }
+        }
+    }
+}
+
+.LayoutSidebar {
+    .content .BannerSearch__title {
+        font-size: ${this._fontSize * 7.2}px;
+    }
+
+    .sidebar .CardTitle {
+        &__title {
+            font-size: ${this._fontSize * 2}px;
+        }
+    }
+}
+
+.BannerSet .CardTitle {
+    &__title {
+        font-size: ${this._fontSize * 5.4}px;
+    }
+}
+
+.BannerDynamic {
+    font-size: ${this._fontSize * 2}px;
+}
+`;
         return _writeToSandboxFile(scss, 'hack.scss');
+    }
+
+    _createWebSCSS() {
+        const webSCSS = `
+html, body {
+    font-size: ${this._fontSize * 2.2}px;
+}
+`;
+        return _writeToSandboxFile(webSCSS, 'hack_web.scss');
     }
 
     _createYAML() {
@@ -150,11 +339,13 @@ $logo-font: 'Patrick Hand SC';
     async launch() {
         const scssFile = await this._createSCSS();
         const yamlFile = await this._createYAML();
+        const webFile = await this._createWebSCSS();
         const proc = new Gio.Subprocess({
             argv: [
                 'com.endlessm.dinosaurs.en',
                 '-J', `${_SANDBOX_PATH}/hack.yaml`,
                 '-O', `${_SANDBOX_PATH}/hack.scss`,
+                '-w', `${_SANDBOX_PATH}/hack_web.scss`,
             ],
         });
         proc.init(null);
@@ -163,6 +354,7 @@ $logo-font: 'Patrick Hand SC';
         } finally {
             scssFile.delete_async(GLib.PRIORITY_LOW, null);
             yamlFile.delete_async(GLib.PRIORITY_LOW, null);
+            webFile.delete_async(GLib.PRIORITY_LOW, null);
         }
     }
 });
